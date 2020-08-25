6e38567017b09edf70befa0cfc8c7afe37c7cefc
ayoubayoubbengara <44469769+ayoubayoubbengara@users.noreply.github.com>
2020-08-25 23:13:15 +0100

    services and type of paiement pge
    i make the type of services page and also the type of paiement and transfire page

---
+++ assets/services.php
@@ -0,0 +1,76 @@
+
+<?php include_once('sections/header.php'); ?>
+
+<!-- Nos services -->
+
+<div class="services">
+  <div class="titleg col-lg-12">
+    <h2>Nos Services</h2>
+    <hr class="line-heading">
+  </div>
+  <div class="img-prg">
+    <div class="imgall">
+      <img class="imgs" src="images/img1.jpg">
+    </div>
+    <div>
+      <p class="prg">test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph</p>
+    </div>
+  </div>
+</div>
+
+<div class="services2">
+  <div class="titleg col-lg-12">
+    <h2>Nos Services</h2>
+    <hr class="line-heading">
+  </div>
+  <div class="img-prg">
+    <div class="imgall">
+      <img class="imgs" src="images/img1.jpg">
+    </div>
+    <div>
+      <p class="prg">test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph</p>
+    </div>                
+    <div class="imgall">
+       
+    </div>
+    <div>
+      <p class="prg">test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph</p>
+    </div>
+  </div>
+</div>
+
+<div class="services2">
+  <div class="titleg col-lg-12">
+    <h2>Nos Services</h2>
+    <hr class="line-heading">
+  </div>
+  <div class="img-prg">
+    <div class="imgall">
+      <img class="imgs" src="images/img1.jpg">
+    </div>
+    <div>
+      <p class="prg">test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph</p>
+    </div>
+      </div>
+</div>
+
+<div class="services2">
+  <div class="titleg col-lg-12">
+    <h2>Nos Services</h2>
+    <hr class="line-heading">
+  </div>
+  <div class="img-prg">
+    <div class="imgall">
+      <img class="imgs" src="images/img1.jpg">
+    </div>
+    <div>
+      <p class="prg">test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph</p>
+    </div>
+  </div>
+</div>
+
+
+
+<!-- end of Nos services -->
+
+<?php include_once('sections/footer.php'); ?>
---
+++ assets/transfert&paiement.php
@@ -0,0 +1,23 @@
+<?php include_once('sections/header.php'); ?>
+<!-- transfert and paiment page -->
+<div class="tran-paie">
+    <div class="titleg col-lg-12">
+        <h2>Nos Services</h2>
+        <hr class="line-heading">
+    </div>
+    <div class="prgv2">
+        <p>
+        test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph
+        </p>
+    </div>
+    <div class="logoimg">
+        <div><img class="pymlogo" src="images/pymimg.png"></div>
+        <div><img class="pymlogo" src="images/pymimg.png"></div>
+        <div><img class="pymlogo" src="images/pymimg.png"></div>
+        <div><img class="pymlogo" src="images/pymimg.png"></div>
+        <div><img class="pymlogo" src="images/pymimg.png"></div>
+        <div><img class="pymlogo" src="images/pymimg.png"></div>
+    </div>
+</div>
+<!-- end of transfert and paiment page -->
+<?php include_once('sections/footer.php'); ?>
